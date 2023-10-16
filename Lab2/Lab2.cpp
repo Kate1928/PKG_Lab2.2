ef7255191ea0b4ca18ce242d890bcda6fc8698b8
Kate1928 <malininaekaterina8005@gmail.com>
2023-10-16 17:59:35 +0300

    Dop/New name

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -1,7 +1,7 @@
 ﻿// Lab2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 
-////--------------------------------------BASE-----------------------------------------
-//
+//-----------------------------------------BASE-----------------------------------------
+
 //#include <SFML/Graphics.hpp>
 //#include <windows.h>
 //#include <fstream>
@@ -32,7 +32,7 @@
 //
 //
 //
-//int getColor(vector<unsigned int>& ind, unsigned int header, int x)
+//int getIndex(vector<unsigned int>& ind, unsigned int header, int x)
 //{
 //    unsigned int pixel = 0;
 //    for (int i = 0; i < header; i++)
@@ -71,12 +71,12 @@
 //    file.get(byte1);
 //    file.get(byte2);
 //
-//    header[0] = byte1 << 16;
+//    header[0] = byte1 << 8;
 //    header[0] = header[0] | byte2;
 //
 //    file.get(byte1);
 //    file.get(byte2);
-//    header[1] = byte1 << 16;
+//    header[1] = byte1 << 8;
 //    header[1] = header[1] | byte2;
 //
 //    file.get(byte1);
@@ -84,7 +84,7 @@
 //
 //    file.get(byte1);
 //    file.get(byte2);
-//    header[3] = byte1 << 16;
+//    header[3] = byte1 << 8;
 //    header[3] = header[3] | byte2;
 //
 //
@@ -108,7 +108,7 @@
 //    //-------------------get image-------------
 //
 //    vector<unsigned int> ind, indAll;
-//    unsigned int pixel = 0, bit;
+//    unsigned int pixel_index = 0, bit;
 //    //string str;
 //
 //    Texture texture;
@@ -127,14 +127,14 @@
 //    for (int x = 0; x < header[0] * header[1] * 4; x += 4)
 //    {
 //
-//        pixel = getColor(ind, header[2], x);
-//        pixel = pixel >> 1;
-//        if (pixel < header[3])
+//        pixel_index = getIndex(ind, header[2], x);
+//        pixel_index = pixel_index >> 1;
+//        if (pixel_index < header[3])
 //        {
-//            pixels[x] = argbAll[pixel].r;
-//            pixels[x + 1] = argbAll[pixel].g;
-//            pixels[x + 2] = argbAll[pixel].b;
-//            pixels[x + 3] = argbAll[pixel].a;
+//            pixels[x] = argbAll[pixel_index].r;
+//            pixels[x + 1] = argbAll[pixel_index].g;
+//            pixels[x + 2] = argbAll[pixel_index].b;
+//            pixels[x + 3] = argbAll[pixel_index].a;
 //        }
 //        else
 //        {
@@ -164,7 +164,218 @@
 //    return 0;
 //}
 
-//--------------------------------------B1-----------------------------------------
+//-----------------------------------------B6-----------------------------------------------
+
+//#include <SFML/Graphics.hpp>
+//#include <windows.h>
+//#include <fstream>
+//#include <string>
+//#include <vector>
+//#include <iostream>
+//
+//using namespace std;
+//using namespace sf;
+//
+//
+//void getByte(char byte1, vector<unsigned int>& ind)
+//{
+//    unsigned int t, res = 0, base = 256;
+//    t = byte1 & 255;
+//    for (int i = 0; i < 8; i++)
+//    {
+//        res += (t & 1) * (base >>= 1);
+//        t >>= 1;
+//    }
+//    for (int i = 0; i < 8; i++)
+//    {
+//        ind.push_back(res & 1);
+//        res >>= 1;
+//    }
+//    return;
+//}
+//
+//
+//
+//int getIndex(vector<unsigned int>& ind, unsigned int header, int x)
+//{
+//    unsigned int pixel = 0;
+//    for (int i = 0; i < header; i++)
+//    {
+//        pixel = pixel | ind[i];
+//        pixel = pixel << 1;
+//    }
+//    auto begin = ind.cbegin(); // указатель на первый элемент
+//    ind.erase(begin, begin + header); // удаляем с третьего элемента до последнего
+//
+//    return pixel;
+//}
+//
+//struct ARGB
+//{
+//    BYTE a;		// alpha
+//    BYTE r;		// red
+//    BYTE g;		// green
+//    BYTE b;		// blue
+//};
+//
+//
+//
+//int main()
+//{
+//
+//    RenderWindow window(VideoMode(400, 400), "Lab2");
+//
+//    ifstream file;
+//    file.open("File4");
+//    ifstream file2;
+//    file2.open("File3");
+//    char byte1;
+//    char byte2;
+//    unsigned int header[4], leftMove = 0;
+//
+//    //----------------get fileData----------------
+//    file.get(byte1);
+//    file.get(byte2);
+//
+//    header[0] = byte1 << 8;
+//    header[0] = header[0] | byte2;
+//
+//    file.get(byte1);
+//    file.get(byte2);
+//    header[1] = byte1 << 8;
+//    header[1] = header[1] | byte2;
+//
+//    file.get(byte1);
+//    header[2] = byte1;
+//
+//    file.get(byte1);
+//    file.get(byte2);
+//    header[3] = byte1 << 8;
+//    header[3] = header[3] | byte2;
+//
+//
+//    //-----------------get Colors------------------
+//
+//
+////    ARGB* argbAll = new ARGB[header[3]];
+//    BYTE rgb;
+//    for (int i = 0; i < header[3]; i++)
+//    {
+//        file.get(byte1);
+//        argbAll[i].a = byte1;
+//        file.get(byte1);
+//        argbAll[i].r = byte1;
+//        file.get(byte1);
+//        argbAll[i].g = byte1;
+//        file.get(byte1);
+//        argbAll[i].b = byte1;
+//    }
+//    
+//    for (int i = 0; i < 4; i++)
+//    {
+//        file2.get(byte1);
+//    }
+//    file2.get(byte1);
+//    unsigned int pb = byte1;
+//    //int countColors = 0;
+//    file2.get(byte1);
+//    file2.get(byte2);
+//    header[3] = byte1 << 16;
+//    header[3] = header[3] | byte2;
+//    
+//    ARGB* argbAll2 = new ARGB[header[3]];
+//
+//    for (int i = 0; i < header[3]; i++)
+//    {
+//        file2.get(byte1);
+//        argbAll2[i].a = byte1;
+//        file2.get(byte1);
+//        argbAll2[i].r = byte1;
+//        file2.get(byte1);
+//        argbAll2[i].g = byte1;
+//        file2.get(byte1);
+//        argbAll2[i].b = byte1;
+//    }
+//
+//    //-------------------get image-------------
+//
+//    vector<unsigned int> ind, ind2, indAll;
+//    unsigned int pixel_index = 0, pixel2 = 0, bit;
+//    //string str;
+//
+//    Texture texture;
+//    texture.create(header[0], header[1]);
+//
+//    Sprite sprite(texture);
+//
+//    while (file.get(byte1))
+//    {
+//        getByte(byte1, ind);
+//    }
+//    while (file2.get(byte1))
+//    {
+//        getByte(byte1, ind2);
+//    }
+//    int a = 0;    
+//
+//
+//    BYTE* pixels = new BYTE[header[0] * header[1] * 4];
+//
+//    for (int x = 0; x < header[0] * header[1] * 4; x += 4)
+//    {
+//
+//        pixel_index = getIndex(ind, header[2], x);
+//        pixel2 = getIndex(ind2, pb, x);
+//        pixel_index = pixel_index >> 1;
+//        pixel2 = pixel2 >> 1;
+//        if (pixel_index < header[3] && pixel2 < pb)
+//        {
+//            pixels[x] = argbAll[pixel_index].r | argbAll2[pixel2].r;
+//            pixels[x + 1] = argbAll[pixel_index].g | argbAll2[pixel2].g;
+//            pixels[x + 2] = argbAll[pixel_index].b | argbAll2[pixel2].b;
+//            pixels[x + 3] = argbAll[pixel_index].a | argbAll2[pixel2].a;
+//        }
+//        else
+//        {
+//            if (pixel_index >= header[3])
+//            {
+//                pixels[x] = Color::Black.r | argbAll2[pixel2].r;
+//                pixels[x + 1] = Color::Black.g | argbAll2[pixel2].g;
+//                pixels[x + 2] = Color::Black.b | argbAll2[pixel2].b;
+//                pixels[x + 3] = Color::Black.a | argbAll2[pixel2].a;
+//            }
+//            else
+//            {
+//                pixels[x] = Color::Black.r | argbAll[pixel_index].r;
+//                pixels[x + 1] = Color::Black.g | argbAll[pixel_index].g;
+//                pixels[x + 2] = Color::Black.b | argbAll[pixel_index].b;
+//                pixels[x + 3] = Color::Black.a | argbAll[pixel_index].a;
+//            }
+//            
+//        }
+//    }
+//    window.clear(Color::Black);
+//    texture.update(pixels);
+//    sprite.setScale(10.0, 10.0);
+//    window.draw(sprite);
+//    window.display();
+//
+//    while (window.isOpen())
+//    {
+//        Event event;
+//        while (window.pollEvent(event))
+//        {
+//            if (event.type == Event::Closed)
+//                window.close();
+//        }
+//
+//
+//    }
+//    return 0;
+//}
+
+//-----------------------------------------П1-----------------------------------------
+
 
 #include <SFML/Graphics.hpp>
 #include <windows.h>
@@ -196,7 +407,7 @@ void getByte(char byte1, vector<unsigned int>& ind)
 
 
 
-int getColor(vector<unsigned int>& ind, unsigned int header, int x)
+int getIndex(vector<unsigned int>& ind, unsigned int header, int x)
 {
     unsigned int pixel = 0;
     for (int i = 0; i < header; i++)
@@ -206,13 +417,8 @@ int getColor(vector<unsigned int>& ind, unsigned int header, int x)
     }
     auto begin = ind.cbegin(); // указатель на первый элемент
     ind.erase(begin, begin + header); // удаляем с третьего элемента до последнего
-
+    
     return pixel;
-}
-
-BYTE brightness(BYTE r, BYTE g, BYTE b)
-{
-    return 0.299 * r + 0.587 * g + 0.114 * b;
 }
 
 struct ARGB
@@ -240,12 +446,12 @@ int main()
     file.get(byte1);
     file.get(byte2);
 
-    header[0] = byte1 << 16;
+    header[0] = byte1 << 8;
     header[0] = header[0] | byte2;
 
     file.get(byte1);
     file.get(byte2);
-    header[1] = byte1 << 16;
+    header[1] = byte1 << 8;
     header[1] = header[1] | byte2;
 
     file.get(byte1);
@@ -253,15 +459,17 @@ int main()
 
     file.get(byte1);
     file.get(byte2);
-    header[3] = byte1 << 16;
+    header[3] = byte1 << 8;
     header[3] = header[3] | byte2;
 
+    
 
     //-----------------get Colors------------------
 
 
     ARGB* argbAll = new ARGB[header[3]];
     BYTE rgb;
+    
     for (int i = 0; i < header[3]; i++)
     {
         file.get(byte1);
@@ -273,12 +481,53 @@ int main()
         file.get(byte1);
         argbAll[i].b = byte1;
     }
+    //------------------get new bmp-------------
 
-    //-------------------get image-------------
+    ofstream fileOut ("FileOut.bmp", ios::binary);
+
+    // записываем заголовок файла
+    unsigned int y = 19778;
+    //fileOut.seekg(0, ios::beg);
+
+    fileOut.write((char*)&y, 2);
+    int x = 4; // number to round up
+    int sizeX = ((header[0] * 3 + x - 1) & ~(x - 1));
+    int size = sizeX * header[1];
+    y = size + 54;
+    fileOut.write((char*)&y, 4);
+    y = 0;
+    fileOut.write((char*)&y, 4);
+    y = 54;
+    fileOut.write((char*)&y, 4);
+
+    // записываем заголовок изображения
+
+
+    y = 40;
+    fileOut.write((char*)&y, 4);
+    fileOut.write((char*)&header[0], 4);
+    fileOut.write((char*)&header[1], 4);
+    y = 1;
+    fileOut.write((char*)&y, 2);
+    //fileOut.write(0, 1);
+    y = 24;
+    fileOut.write((char*)&y, 2);
+    y = 0;
+    fileOut.write((char*)&y, 4);
+
+    fileOut.write((char*)&size, 4);
+    y = 2835;
+    fileOut.write((char*)&y, 4);
+    fileOut.write((char*)&y, 4);
+    y = 0;
+    fileOut.write((char*)&y, 4);
+    fileOut.write((char*)&y, 4);
+
+
+    //-------------------get image-----+new bmp--------
 
     vector<unsigned int> ind, indAll;
-    unsigned int pixel = 0, bit;
-    //string str;
+    unsigned int pixel_index = 0, bit;
 
     Texture texture;
     texture.create(header[0], header[1]);
@@ -289,25 +538,22 @@ int main()
     {
         getByte(byte1, ind);
     }
-    int a = 0;
+    
 
     BYTE* pixels = new BYTE[header[0] * header[1] * 4];
-    BYTE bri = 0;
-    vector<int> allPixels;
 
     for (int x = 0; x < header[0] * header[1] * 4; x += 4)
     {
-        
-        pixel = getColor(ind, header[2], x);
-        pixel = pixel >> 1;
-        bri += brightness(argbAll[pixel].r, argbAll[pixel].g, argbAll[pixel].b);// all brightness
-        allPixels.push_back(pixel);
-        /*if (pixel < header[3])
+
+        pixel_index = getIndex(ind, header[2], x);
+        pixel_index = pixel_index >> 1;
+        if (pixel_index < header[3])
         {
-            pixels[x] = argbAll[pixel].r;
-            pixels[x + 1] = argbAll[pixel].g;
-            pixels[x + 2] = argbAll[pixel].b;
-            pixels[x + 3] = argbAll[pixel].a;
+            pixels[x] = argbAll[pixel_index].r;
+            pixels[x + 1] = argbAll[pixel_index].g;
+            pixels[x + 2] = argbAll[pixel_index].b;
+            pixels[x + 3] = argbAll[pixel_index].a;
+
         }
         else
         {
@@ -315,30 +561,55 @@ int main()
             pixels[x + 1] = Color::Black.g;
             pixels[x + 2] = Color::Black.b;
             pixels[x + 3] = Color::Black.a;
+        }
+        /*if (a < header[0] - 1)
+        {
+            fileOut.write((char*)&pixels[x + 2], 1);
+            fileOut.write((char*)&pixels[x + 1], 1);
+            fileOut.write((char*)&pixels[x], 1);
+            a++;
+        }
+
+        if (a == header[0] - 1)
+        {
+            fileOut.write((char*)&pixels[x + 2], 1);
+            fileOut.write((char*)&pixels[x + 1], 1);
+            fileOut.write((char*)&pixels[x], 1);
+            y = 0;
+            fileOut.write((char*)&y, (size / header[1]) - header[0]);
+            a = 0;
         }*/
+        
     }
-    //средняя яркость 
-    bri /= header[0] * header[1];
-    cout << "Enter a contrast level:  ";
-    int contrastLevel;
-    cin >> contrastLevel;
-    //Коэффициент коррекции
-    double k = 1.0 + contrastLevel / 100.0;
-    for (int i = 0; i < header[3]; i++)
+
+    BYTE** pixelBMP = new BYTE * [header[1]];
+    for (int i = 0; i < header[1]; i++)
     {
-        int delta = i - bri;
-        int temp = bri + k * delta;
+        pixelBMP[i] = new BYTE[header[0] * 4];
+    }
+    int a = 0;
+    for (int i = 0; i < header[1]; i++)
+    {
+        for (int j = 0; j < header[0] * 4; j++)
+        { 
+            pixelBMP[i][j] = pixels[a++];
+        }
+    }
 
-        if (temp < 0)
-            temp = 0;
-
-        if (temp >= 255)
-            temp = 255;
-        argbAll[i] = (unsigned char)temp;
+    for (int i = header[1] - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < header[0] * 4; j += 4)
+        {
+            fileOut.write((char*)&pixelBMP[i][j + 2], 1);//b
+            fileOut.write((char*)&pixelBMP[i][j + 1], 1);//g
+            fileOut.write((char*)&pixelBMP[i][j], 1);//r
+        }
+        y = 0;
+        fileOut.write((char*)&y, sizeX - (header[0] * 3));
     }
     window.clear(Color::Black);
     texture.update(pixels);
-    sprite.setScale(10.0, 10.0);
+    //sprite.setScale(10.0, 10.0);
     window.draw(sprite);
     window.display();
 
@@ -349,11 +620,9 @@ int main()
         {
             if (event.type == Event::Closed)
                 window.close();
-        }
-
-
+        }  
     }
+
+   
     return 0;
 }
-
-
--- PKG2/PKG2/PKG2.cpp
+++ PKG2/PKG2/PKG2.cpp
@@ -1,7 +1,7 @@
 ﻿//// PKG2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 
 
-////--------------------------------------------------BASE---------------------------------------------------
+//--------------------------------------------------BASE---------------------------------------------------
 
 //#include <SFML/Graphics.hpp>
 //#include <windows.h>
@@ -33,7 +33,7 @@
 //
 //
 //
-//int getColor(vector<unsigned int>& ind, unsigned int header, int x)
+//int getIndex(vector<unsigned int>& ind, unsigned int header, int x)
 //{
 //    unsigned int pixel = 0;
 //    for (int i = 0; i < header; i++)
@@ -73,12 +73,12 @@
 //    file.get(byte1);
 //    file.get(byte2);
 //
-//    header[0] = byte1 << 16;
+//    header[0] = byte1 << 8;
 //    header[0] = header[0] | byte2;
 //
 //    file.get(byte1);
 //    file.get(byte2);
-//    header[1] = byte1 << 16;
+//    header[1] = byte1 << 8;
 //    header[1] = header[1] | byte2;
 //
 //    file.get(byte1);
@@ -86,7 +86,7 @@
 //
 //    file.get(byte1);
 //    file.get(byte2);
-//    header[3] = byte1 << 16;
+//    header[3] = byte1 << 8;
 //    header[3] = header[3] | byte2;
 //
 //
@@ -111,7 +111,7 @@
 //    //-------------------get image-------------
 //
 //    vector<unsigned int> ind, indAll;
-//    unsigned int pixel = 0, bit;
+//    unsigned int pixel_index = 0, bit;
 //    //string str;
 //
 //    Texture texture;
@@ -130,14 +130,14 @@
 //    for (int x = 0; x < header[0] * header[1] * 4; x += 4)
 //    {
 //
-//        pixel = getColor(ind, header[2], x);
-//        pixel = pixel >> 1;
-//        if (pixel < header[3])
+//        pixel_index = getIndex(ind, header[2], x);
+//        pixel_index = pixel_index >> 1;
+//        if (pixel_index < header[3])
 //        {
-//            pixels[x] = argbAll[pixel].r;
-//            pixels[x + 1] = argbAll[pixel].g;
-//            pixels[x + 2] = argbAll[pixel].b;
-//            pixels[x + 3] = argbAll[pixel].a;
+//            pixels[x] = argbAll[pixel_index].r;
+//            pixels[x + 1] = argbAll[pixel_index].g;
+//            pixels[x + 2] = argbAll[pixel_index].b;
+//            pixels[x + 3] = argbAll[pixel_index].a;
 //        }
 //        else
 //        {
@@ -167,9 +167,197 @@
 //    return 0;
 //}
 
-////---------------------------------------------------B4-----------------------------------------------
+//---------------------------------------------------B4-----------------------------------------------
 
 // PKG2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+#include <SFML/Graphics.hpp>
+#include <windows.h>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+using namespace sf;
+
+
+void getByte(char byte1, vector<unsigned int>& ind)
+{
+    unsigned int t, res = 0, base = 256;
+    t = byte1 & 255;
+    for (int i = 0; i < 8; i++)
+    {
+        res += (t & 1) * (base >>= 1);
+        t >>= 1;
+    }
+    for (int i = 0; i < 8; i++)
+    {
+        ind.push_back(res & 1);
+        res >>= 1;
+    }
+    return;
+}
+
+
+
+int getIndex(vector<unsigned int>& ind, unsigned int header, int x)
+{
+    unsigned int pixel = 0;
+    for (int i = 0; i < header; i++)
+    {
+        pixel = pixel | ind[i];
+        pixel = pixel << 1;
+    }
+    auto begin = ind.cbegin(); // указатель на первый элемент
+    ind.erase(begin, begin + header); // удаляем с третьего элемента до последнего
+
+    return pixel;
+}
+
+struct ARGB
+{
+    BYTE a;		// alpha
+    BYTE r;		// red
+    BYTE g;		// green
+    BYTE b;		// blue
+};
+
+
+
+int main()
+{
+
+    RenderWindow window(VideoMode(400, 400), "Lab2");
+
+    ifstream file;
+    file.open("File4");
+    char byte1;
+    char byte2;
+    unsigned int header[4], leftMove = 0;
+
+    /* cout << "No file specified\n";
+     cin >> leftMove;*/
+//    cin >> hex >> leftMove;
+    //----------------get fileData----------------
+    file.get(byte1);
+    file.get(byte2);
+
+    header[0] = byte1 << 8;
+    header[0] = header[0] | byte2;
+
+    file.get(byte1);
+    file.get(byte2);
+    header[1] = byte1 << 8;
+    header[1] = header[1] | byte2;
+
+    file.get(byte1);
+    header[2] = byte1;
+
+    file.get(byte1);
+    file.get(byte2);
+    header[3] = byte1 << 8;
+    header[3] = header[3] | byte2;
+
+
+    //-----------------get Colors------------------
+
+
+    ARGB* argbAll = new ARGB[header[3]];
+    BYTE rgb;
+    for (int i = 0; i < header[3]; i++)
+    {
+        file.get(byte1);
+        argbAll[i].a = byte1;
+        file.get(byte1);
+        argbAll[i].r = byte1;
+        file.get(byte1);
+        argbAll[i].g = byte1;
+        file.get(byte1);
+        argbAll[i].b = byte1;
+    }
+    
+
+    //-------------------get image-------------
+
+    vector<unsigned int> ind, indAll;
+    unsigned int pixel_index = 0, bit;
+    //string str;
+
+    Texture texture;
+    texture.create(header[0], header[1]);
+
+    Sprite sprite(texture);
+
+    while (file.get(byte1))
+    {
+        getByte(byte1, ind);
+    }
+    int a = 0;
+
+    BYTE newARGB;
+    unsigned long int newColor;
+    bool choise;
+    cout << "Change the color? 1-yes/0-no ";
+    cin >> choise;
+    if (choise)
+    {
+         cout << endl << "Enter a new color in hexademical format(ARGB):  ";
+        cin >> hex >> newColor;
+        argbAll[0].b = newColor & 255;
+        newColor = newColor >> 8;
+        argbAll[0].g = newColor & 255;
+        newColor = newColor >> 8;
+        argbAll[0].r = newColor & 255;
+        newColor = newColor >> 8;
+        argbAll[0].a = newColor & 255;
+    }
+   
+
+    BYTE* pixels = new BYTE[header[0] * header[1] * 4];
+
+    for (int x = 0; x < header[0] * header[1] * 4; x += 4)
+    {
+
+        pixel_index = getIndex(ind, header[2], x);
+        pixel_index = pixel_index >> 1;
+        if (pixel_index < header[3])
+        {
+            pixels[x] = argbAll[pixel_index].r;
+            pixels[x + 1] = argbAll[pixel_index].g;
+            pixels[x + 2] = argbAll[pixel_index].b;
+            pixels[x + 3] = argbAll[pixel_index].a;
+        }
+        else
+        {
+            pixels[x] = Color::Black.r;
+            pixels[x + 1] = Color::Black.g;
+            pixels[x + 2] = Color::Black.b;
+            pixels[x + 3] = Color::Black.a;
+        }
+    }
+    window.clear(Color::Black);
+    texture.update(pixels);
+    sprite.setScale(10.0, 10.0);
+    window.draw(sprite);
+    window.display();
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+
+    }
+    return 0;
+}
+
+//-----------------------------------------------------П6---------------------------------------------------
+
+//
 //#include <SFML/Graphics.hpp>
 //#include <windows.h>
 //#include <fstream>
@@ -200,192 +388,7 @@
 //
 //
 //
-//int getColor(vector<unsigned int>& ind, unsigned int header, int x)
-//{
-//    unsigned int pixel = 0;
-//    for (int i = 0; i < header; i++)
-//    {
-//        pixel = pixel | ind[i];
-//        pixel = pixel << 1;
-//    }
-//    auto begin = ind.cbegin(); // указатель на первый элемент
-//    ind.erase(begin, begin + header); // удаляем с третьего элемента до последнего
-//
-//    return pixel;
-//}
-//
-//struct ARGB
-//{
-//    BYTE a;		// alpha
-//    BYTE r;		// red
-//    BYTE g;		// green
-//    BYTE b;		// blue
-//};
-//
-//
-//
-//int main()
-//{
-//
-//    RenderWindow window(VideoMode(400, 400), "Lab2");
-//
-//    ifstream file;
-//    file.open("File4");
-//    char byte1;
-//    char byte2;
-//    unsigned int header[4], leftMove = 0;
-//
-//    /* cout << "No file specified\n";
-//     cin >> leftMove;*/
-////    cin >> hex >> leftMove;
-//    //----------------get fileData----------------
-//    file.get(byte1);
-//    file.get(byte2);
-//
-//    header[0] = byte1 << 16;
-//    header[0] = header[0] | byte2;
-//
-//    file.get(byte1);
-//    file.get(byte2);
-//    header[1] = byte1 << 16;
-//    header[1] = header[1] | byte2;
-//
-//    file.get(byte1);
-//    header[2] = byte1;
-//
-//    file.get(byte1);
-//    file.get(byte2);
-//    header[3] = byte1 << 16;
-//    header[3] = header[3] | byte2;
-//
-//
-//    //-----------------get Colors------------------
-//
-//
-//    ARGB* argbAll = new ARGB[header[3]];
-//    BYTE rgb;
-//    for (int i = 0; i < header[3]; i++)
-//    {
-//        file.get(byte1);
-//        argbAll[i].a = byte1;
-//        file.get(byte1);
-//        argbAll[i].r = byte1;
-//        file.get(byte1);
-//        argbAll[i].g = byte1;
-//        file.get(byte1);
-//        argbAll[i].b = byte1;
-//    }
-//    
-//
-//    //-------------------get image-------------
-//
-//    vector<unsigned int> ind, indAll;
-//    unsigned int pixel = 0, bit;
-//    //string str;
-//
-//    Texture texture;
-//    texture.create(header[0], header[1]);
-//
-//    Sprite sprite(texture);
-//
-//    while (file.get(byte1))
-//    {
-//        getByte(byte1, ind);
-//    }
-//    int a = 0;
-//
-//    BYTE newARGB;
-//    unsigned long int newColor;
-//    //cout << "Change the color? 1-yes/0-no ";
-//    //cin >> choise;
-//    cout << endl << "Enter a new color in hexademical format(ARGB):  ";
-//    cin >> hex >> newColor;
-//    argbAll[0].b = newColor & 255;
-//    newColor = newColor >> 8;
-//    argbAll[0].g = newColor & 255;
-//    newColor = newColor >> 8;
-//    argbAll[0].r = newColor & 255;
-//    newColor = newColor >> 8;
-//    argbAll[0].a = newColor & 255;
-//    newColor = newColor >> 8;
-//
-//
-//    BYTE* pixels = new BYTE[header[0] * header[1] * 4];
-//
-//    for (int x = 0; x < header[0] * header[1] * 4; x += 4)
-//    {
-//
-//        pixel = getColor(ind, header[2], x);
-//        pixel = pixel >> 1;
-//        if (pixel < header[3])
-//        {
-//            pixels[x] = argbAll[pixel].r;
-//            pixels[x + 1] = argbAll[pixel].g;
-//            pixels[x + 2] = argbAll[pixel].b;
-//            pixels[x + 3] = argbAll[pixel].a;
-//        }
-//        else
-//        {
-//            pixels[x] = Color::Black.r;
-//            pixels[x + 1] = Color::Black.g;
-//            pixels[x + 2] = Color::Black.b;
-//            pixels[x + 3] = Color::Black.a;
-//        }
-//    }
-//    window.clear(Color::Black);
-//    texture.update(pixels);
-//    sprite.setScale(10.0, 10.0);
-//    window.draw(sprite);
-//    window.display();
-//
-//    while (window.isOpen())
-//    {
-//        Event event;
-//        while (window.pollEvent(event))
-//        {
-//            if (event.type == Event::Closed)
-//                window.close();
-//        }
-//
-//
-//    }
-//    return 0;
-//}
-
-//-----------------------------------------------------П4---------------------------------------------------
-
-//
-//#include <SFML/Graphics.hpp>
-//#include <windows.h>
-//#include <fstream>
-//#include <string>
-//#include <vector>
-//#include <iostream>
-//
-//using namespace std;
-//using namespace sf;
-//
-//
-//void getByte(char byte1, vector<unsigned int>& ind)
-//{
-//    unsigned int t, res = 0, base = 256;
-//    t = byte1 & 255;
-//    for (int i = 0; i < 8; i++)
-//    {
-//        res += (t & 1) * (base >>= 1);
-//        t >>= 1;
-//    }
-//    for (int i = 0; i < 8; i++)
-//    {
-//        ind.push_back(res & 1);
-//        res >>= 1;
-//    }
-//    return;
-//}
-//
-//
-//
-//int getColor(vector<unsigned int>& ind, unsigned int header, int x)
+//int getIndex(vector<unsigned int>& ind, unsigned int header, int x)
 //{
 //    unsigned int pixel = 0;
 //    for (int i = 0; i < header; i++)
@@ -427,12 +430,12 @@
 //    file.get(byte1);
 //    file.get(byte2);
 //
-//    header[0] = byte1 << 16;
+//    header[0] = byte1 << 8;
 //    header[0] = header[0] | byte2;
 //
 //    file.get(byte1);
 //    file.get(byte2);
-//    header[1] = byte1 << 16;
+//    header[1] = byte1 << 8;
 //    header[1] = header[1] | byte2;
 //
 //    file.get(byte1);
@@ -440,7 +443,7 @@
 //
 //    file.get(byte1);
 //    file.get(byte2);
-//    header[3] = byte1 << 16;
+//    header[3] = byte1 << 8;
 //    header[3] = header[3] | byte2;
 //
 //
@@ -465,7 +468,7 @@
 //    //-------------------get image-------------
 //
 //    vector<unsigned int> ind, indAll;
-//    unsigned int pixel = 0, bit;
+//    unsigned int pixel_index = 0, bit;
 //    //string str;
 //
 //    Texture texture;
@@ -505,14 +508,14 @@
 //    for (int x = 0; x < header[0] * header[1] * 4; x += 4)
 //    {
 //
-//        pixel = getColor(ind, header[2], x);
-//        pixel = pixel >> 1;
-//        if (pixel < header[3])
+//        pixel_index = getIndex(ind, header[2], x);
+//        pixel_index = pixel_index >> 1;
+//        if (pixel_index < header[3])
 //        {
-//            pixels[x] = argbAll[pixel].r;
-//            pixels[x + 1] = argbAll[pixel].g;
-//            pixels[x + 2] = argbAll[pixel].b;
-//            pixels[x + 3] = argbAll[pixel].a;
+//            pixels[x] = argbAll[pixel_index].r;
+//            pixels[x + 1] = argbAll[pixel_index].g;
+//            pixels[x + 2] = argbAll[pixel_index].b;
+//            pixels[x + 3] = argbAll[pixel_index].a;
 //        }
 //        else
 //        {
